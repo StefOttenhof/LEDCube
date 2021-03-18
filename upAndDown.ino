4ef234febc94dbbc0599c5f5dd810b19ce10cd61
Stef Ottenhof <stefottenhof@hotmail.com>
2021-03-19 00:53:02 +0100

    Added comments upAndDown.ino

--- upAndDown.ino
+++ upAndDown.ino
@@ -6,19 +6,31 @@ int groundUpArray[7] = { FIRSTGROUND, SECONDGROUND, THIRDGROUND, FOURTHGROUND, T
 int groundDownArray[7] = { FOURTHGROUND, THIRDGROUND, SECONDGROUND, FIRSTGROUND, SECONDGROUND, THIRDGROUND, FOURTHGROUND };
 
 void upAndDownAnimation(bitDirection animationDirection, lastCycle cycle){
+    /*
+  * Function: upAndDownAnimation
+  * ----------------------------
+  *   Animation of 16 bits going from the bottom to the top of the cube
+  *
+  *   animationDirection: Determines the inital direction of the animation
+  *   
+  *   Cycle: Makes the animation look smoother when played multiple times by eliminating the last state
+  *   
+  *   returns: none
+  */
   turnAllLayersOff();
   firstByte = 0B11111111;
   secondByte = 0B11111111;
   shiftData(MSB);
 
-  // Select direction
+  /*
+  * Copy one of two arrays to animationArray
+  */
   if(animationDirection == LSB){
     memcpy(animationArray, groundUpArray, sizeof(groundUpArray[0])*7);
   } else {
     memcpy(animationArray, groundDownArray, sizeof(groundDownArray[0])*7);
   }
 
-  // Loop through ground layers
   for(int i = 0; i < 6 + cycle; i++){
     if(i == 0){
       turnLayerOn(animationArray[i]);
