acab10f058590609b63933331c16491dd7275681
Stef Ottenhof <stefottenhof@hotmail.com>
2021-03-19 00:00:56 +0100

    Added comments forwardAndBackward.ino

--- forwardAndBackward.ino
+++ forwardAndBackward.ino
@@ -2,83 +2,102 @@
 #include "Config.h"
 
 void forwardAndBackwardAnimation(bitDirection animationDirection, lastCycle cycle){
-  // Set all layers to high
+  /*
+  * Function: circleAnimation
+  * ----------------------------
+  *   Animation of 4 bits going from the front to the back of the cube
+  *
+  *   animationDirection: Determines the inital direction of the animation
+  *   
+  *   Cycle: Makes the animation look smoother when played multiple times by eliminating the last state
+  *   
+  *   returns: none
+  */
   turnAllLayersOn();
 
   if(animationDirection == MSB){
-    // Set bit pattern
+    /*
+    * Move forward
+    */
     firstByte = 0B00001111;
-    secondByte = 0B00000000;
-
-    // Loop 
+    secondByte = 0B00000000
+    
     for(int i = 0; i < 4; i++){
       shiftData(animationDirection);
 
       if(firstByte == 0B11110000){
         firstByte = 0B00000000;
         secondByte = 0B00001111;
+      
       } else {
         secondByte = secondByte << 4;
         firstByte = firstByte << 4;
+        
       }
-
       shiftDelay();
     }
 
-    // Set bit pattern
+    /*
+    * Move backwards
+    */
     firstByte = 0B00000000;
     secondByte = 0B00001111;
 
-    // Loop 
     for(int i = 0; i < 2 + cycle; i++){
       shiftData(animationDirection);
 
       if(secondByte == 0B00001111){
         secondByte = 0B00000000;
         firstByte = 0B11110000;
+      
       } else {
         secondByte = secondByte >> 4;
         firstByte = firstByte >> 4;
+        
       }
-
       shiftDelay();
     }
+  
   } else {
-    // Set bit pattern
+    /*
+    * Move backwards
+    */
     firstByte = 0B00000000;
     secondByte = 0B00001111;
 
-    // Loop 
     for(int i = 0; i < 4; i++){
       shiftData(animationDirection);
 
       if(secondByte == 0B11110000){
         secondByte = 0B00000000;
         firstByte = 0B00001111;
+      
       } else {
         secondByte = secondByte << 4;
         firstByte = firstByte << 4;
+        
       }
-
       shiftDelay();
     }
 
-    // Set bit pattern
+    /*
+    * Move forward
+    */
     firstByte = 0B00001111;
     secondByte = 0B00000000;
 
-    // Loop 
     for(int i = 0; i < 2 + cycle; i++){
       shiftData(animationDirection);
 
       if(firstByte == 0B00001111){
         firstByte = 0B00000000;
         secondByte = 0B11110000;
+      
       } else {
         secondByte = secondByte >> 4;
         firstByte = firstByte >> 4;
+        
       }
-
       shiftDelay();
     }
   }
