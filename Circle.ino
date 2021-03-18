93193b6a11f3161be932973acb3511b21c1ab9ba
Stef Ottenhof <stefottenhof@hotmail.com>
2021-03-18 23:52:37 +0100

    Added comments Circle.ino

--- Circle.ino
+++ Circle.ino
@@ -1,31 +1,43 @@
 /* CICLE_INO */
 #include "Config.h"
 
-int indexesLsb[12] = { 0B11100000, 0B01110000, 0B00110001, 0B00010001, 0B00000001, 0B00000000, 0B00000000, 0B00000000, 0B00000000, 0B00001000, 0B10001000, 0B11001000};
-int indexesMsb[12] = { 0B00000000, 0B00000000, 0B00000000, 0B00010000, 0B00010001, 0B00010011, 0B00000111, 0B00001110, 0B10001100, 0B10001000, 0B10000000, 0B00000000};
+/*
+* Byte values for both halves of the cube
+*/
+int indexesSecondByte[12] = { 0B11100000, 0B01110000, 0B00110001, 0B00010001, 0B00000001, 0B00000000, 0B00000000, 0B00000000, 0B00000000, 0B00001000, 0B10001000, 0B11001000};
+int indexesFirstByte[12] = { 0B00000000, 0B00000000, 0B00000000, 0B00010000, 0B00010001, 0B00010011, 0B00000111, 0B00001110, 0B10001100, 0B10001000, 0B10000000, 0B00000000};
 
 void circleAnimation(bitDirection animationDirection, lastCycle cycle){
-   // Set all layers to high
+  /*
+  * Function: circleAnimation
+  * ----------------------------
+  *   Animation of 3 bits going around the outter ring
+  *
+  *   animationDirection: Determines the inital direction of the animation
+  *   
+  *   Cycle: Makes the animation look smoother when played multiple times by eliminating the last state
+  *   
+  *   returns: none
+  */
   turnAllLayersOn();
 
   if(animationDirection == MSB){
     for(int i = 0; i < 12 - cycle; i++){
-       firstByte = indexesMsb[i];
-       secondByte = indexesLsb[i];
-        
-       shiftData(MSB);
-
-       shiftDelay();
+      firstByte = indexesFirstByte[i];
+      secondByte = indexesSecondByte[i]; 
+      shiftData(MSB);
+      shiftDelay();
     }
+  
   } else {
     for(int i = 12; i >= 0 + cycle; i--){
-       firstByte = indexesMsb[i];
-       secondByte = indexesLsb[i];
-        
-       shiftData(MSB);
-
-       shiftDelay();
+      firstByte = indexesFirstByte[i];
+      secondByte = indexesSecondByte[i];
+      shiftData(MSB);
+      shiftDelay();
     }
+    
   }
 }
+
 /* END CIRCLE_INO */
