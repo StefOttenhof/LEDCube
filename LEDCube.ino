022e93010d7935f63f67463b55e1a3c7b9ac9956
Stef Ottenhof <stefottenhof@hotmail.com>
2021-03-19 01:08:56 +0100

    Added comments LEDCube.ino

--- LEDCube.ino
+++ LEDCube.ino
@@ -26,52 +26,75 @@ void setup(){
 }
 
 ISR (PCINT2_vect){
-     if(!(digitalRead(BUTTONY)) && !ButtonYState && mode == 1){
-     ButtonYState = HIGH;
-     mode = 0;
-     initialLoad = 1;
+  /*
+  * Interup Service Routine
+  * ----------------------------
+  *   Interupt on pin 0, 1 and 2
+  *   Switches from mode and controls one LED in cube
+  *   
+  */
+  if(!(digitalRead(BUTTONY)) && !ButtonYState && mode == 1){
+    ButtonYState = HIGH;
+    mode = 0;
+    initialLoad = 1;
      
-     } else if(digitalRead(BUTTONY) && ButtonYState){
-       ButtonYState = LOW;
-     }
+   } else if(digitalRead(BUTTONY) && ButtonYState){
+     ButtonYState = LOW;
+   }
 
-     if(!mode && !playing){
-        if(!(digitalRead(BUTTONX)) && !ButtonXState){
-           ButtonXState = HIGH;
-           moveX();
-     
-         } else if(digitalRead(BUTTONX) && ButtonXState){
-            ButtonXState = LOW;
-         }
+   if(!mode && !playing){
+      if(!(digitalRead(BUTTONX)) && !ButtonXState){
+         ButtonXState = HIGH;
+         moveX();
+   
+       } else if(digitalRead(BUTTONX) && ButtonXState){
+          ButtonXState = LOW;
+       }
 
-        if(!(digitalRead(BUTTONY)) && !ButtonYState){
-            ButtonYState = HIGH;
-            moveY();
-     
-        } else if(digitalRead(BUTTONY) && ButtonYState){
-            ButtonYState = LOW;
-        }
+      if(!(digitalRead(BUTTONY)) && !ButtonYState){
+          ButtonYState = HIGH;
+          moveY();
+   
+      } else if(digitalRead(BUTTONY) && ButtonYState){
+          ButtonYState = LOW;
+      }
 
-        if(!(digitalRead(BUTTONZ)) && !ButtonZState){
-            ButtonZState = HIGH;
-            moveZ();
-     
-         } else if(digitalRead(BUTTONZ) && ButtonZState){
-            ButtonZState = LOW;
-         }
-     }
+      if(!(digitalRead(BUTTONZ)) && !ButtonZState){
+          ButtonZState = HIGH;
+          moveZ();
+   
+       } else if(digitalRead(BUTTONZ) && ButtonZState){
+          ButtonZState = LOW;
+       }
+   }
 }
 
 void playAnimations(){
-    playing = true;
-    
-    leftAndRightAnimation(MSB, YES);
-    forwardAndBackwardAnimation(LSB, YES);
-    circleAnimation(MSB, NO);
-    //twoRowsLeftAndRightAnimation(MSB, YES);
-    upAndDownAnimation(MSB, NO);
-    
-    playing = false;
+  /*
+  * Function: playAnimations
+  * ----------------------------
+  *   Plays a list of animations
+  *   
+  *   returns: none
+  */
+  playing = true;
+
+  /*
+  * Add list of animations below
+  * ⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄
+  */
+  
+  leftAndRightAnimation(MSB, YES);
+  forwardAndBackwardAnimation(LSB, YES);
+  circleAnimation(MSB, NO);
+  //twoRowsLeftAndRightAnimation(MSB, YES);
+  upAndDownAnimation(MSB, NO);
+  
+  /*
+  * ⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃⌃
+  * Add list of animations above
+  */
+  playing = false;
 }
 
 void loop(){
